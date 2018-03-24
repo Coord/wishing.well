f5608539d09c53edef58138bdc2e1c8f2eb99a7b
Daniel Blackburn <craklyn@gmail.com>
2018-03-24 12:05:11 -0700

    Initial commit of smartplug code

---
+++ smartplug/index.js
@@ -0,0 +1,19 @@
+var TPLink = require('tplink-cloud-api')
+var uuidV4 = require('uuid/v4')
+
+async function init() {
+  let myTPLink = await TPLink.login('craklyn@gmail.com', 'fountainPW');
+  let deviceList = await myTPLink.getDeviceList();
+  //console.log(deviceList);
+  
+  myPlug = myTPLink.getHS110("The Fountain");
+  console.log(myPlug);
+  myPlug.toggle();
+  
+}
+
+init();
+
+//let deviceList = await myTPLink.getDeviceList()
+
+//let deviceName = "The fountain"
