7e82dc5a8c0bb48e2f3bcbbc70751d0ad0f241c7
Daniel Blackburn <craklyn@gmail.com>
2018-03-24 13:46:37 -0700

    updated api

--- smartplug/index.js
+++ smartplug/index.js
@@ -8,11 +8,31 @@ async function init() {
   
   myPlug = myTPLink.getHS110("The Fountain");
   console.log(myPlug);
-  myPlug.toggle();
-  
+  myPlug.toggle();  
 }
 
-module.exports = init;
+async function toggleOn() {
+  let myTPLink = await TPLink.login('craklyn@gmail.com', 'fountainPW');
+  let deviceList = await myTPLink.getDeviceList();
+  //console.log(deviceList);
+  
+  myPlug = myTPLink.getHS110("The Fountain");
+  console.log(myPlug);
+  myPlug.powerOn();  
+}
+
+async function toggleOff() {
+  let myTPLink = await TPLink.login('craklyn@gmail.com', 'fountainPW');
+  let deviceList = await myTPLink.getDeviceList();
+  //console.log(deviceList);
+  
+  myPlug = myTPLink.getHS110("The Fountain");
+  console.log(myPlug);
+  myPlug.powerOff();  
+}
+
+
+module.exports = {'toggleOn': toggleOn, 'toggleOff': toggleOff};
 
 //let deviceList = await myTPLink.getDeviceList()
 
