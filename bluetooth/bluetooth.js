e1816fb99ae181d9c4ac7d360899fbc2812a6bd3
S. Gallagher <sjgllghr@gmail.com>
2018-03-24 13:05:47 -0700

    loop

--- bluetooth/bluetooth.js
+++ bluetooth/bluetooth.js
@@ -1,23 +1,17 @@
 const bluetooth = require('node-bluetooth');
 const money = require('../money.movement/app');
  
-// create bluetooth device instance
-const device = new bluetooth.DeviceINQ();
+function intervalFunc() {
+  // create bluetooth device instance
+  const device = new bluetooth.DeviceINQ();
 
-//device.listPairedDevices(console.log);
+  device
+  .on('finished',  console.log.bind(console, 'finished'))
+  .on('found', function found(address, name){
+    console.log('Found: ' + address + ' with name ' + name);
+    money();
+  
+  }).inquire();
+}
 
-device
-.on('finished',  console.log.bind(console, 'finished'))
-.on('found', function found(address, name){
-  console.log('Found: ' + address + ' with name ' + name);
-  money();
-//   bluetooth.connect(address, channel, function(err, connection){
-//     if(err) return console.error(err);
-   
-//     connection.on('data', (buffer) => {
-//       console.log('received message:', buffer.toString());
-//     });
-   
-//     connection.write(new Buffer('Hello!', 'utf-8'));
-//   });
-}).inquire();
+setInterval(intervalFunc, 1500);
